88818d7e48a837de4592e852bcfa64105ff6291d
xingxiaodong <1204642219@qq.com>
2018-07-11 07:52:25 +0800

    2.0

--- canvas/main.js
+++ canvas/main.js
@@ -1,7 +1,14 @@
+// document.body.ontouchstart=function(eee){
+//     eee.preventDefault()
+// }
+
+
 
 /*设置画板宽高*/
 var yyy=document.getElementById('xxx');
 var context=yyy.getContext('2d');
+var lineWidth=5
+
 autoSetCanvasSize(yyy)
 
 /*******/
@@ -9,15 +16,55 @@ listenToMouse(yyy)
 
 /*******/
 var eraserEnabled=false
-eraser.onclick=function(){ 
-    eraserEnabled=true
-    actions.className='actions x'
-}
-brush.onclick=function(){ 
+pen.onclick=function(){
     eraserEnabled=false
-    actions.className='actions'
+    pen.classList.add('active')
+    eraser.classList.remove('active')
 }
-
+eraser.onclick=function(){
+    eraserEnabled=true
+    eraser.classList.add('active')
+    pen.classList.remove('active')
+}
+red.onclick=function(){
+    context.strokeStyle='red'
+    context.fillStyle='red'
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+green.onclick=function(){
+    context.strokeStyle='green'
+    context.fillStyle='green'
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+}
+blue.onclick=function(){
+    context.strokeStyle='blue'
+    context.fillStyle='blue'
+    blue.classList.add('active')
+    green.classList.remove('active')
+    red.classList.remove('active')
+}
+clear.onclick=function(){
+    context.clearRect(0,0,yyy.width,yyy.height);
+}
+download.onclick=function(){
+    var url = yyy.toDataURL('image/png')
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href=url
+    a.download='我的画'
+    a.target='_blank'
+    a.click()
+}
+thin.onclick=function(){
+    lineWidth=5;
+}
+thick.onclick=function(){
+    lineWidth=10;
+}
 
 /*******/
 
@@ -93,15 +140,15 @@ function drawCircle(x,y,radius){
     context.beginPath();
     context.arc(x,y,radius,0,Math.PI*2)
     //context.stroke()
-    context.fillStyle='black';
+    //context.fillStyle='black';
     context.fill()
 }
 function drawLine(x1,y1,x2,y2){
     context.beginPath();
     context.moveTo(x1,y1);//起点
-    context.lineWidth=5
+    context.lineWidth=lineWidth
     context.lineTo(x2,y2);//终点
-    context.strokeStyle='black'
+    //context.strokeStyle='black'
     context.stroke();
     context.closePath();
 }  
